48ad98796ae873c9249f6e3ae90cc68851100a8b
hels <m7hels@gmail.com>
2025-09-21 11:19:29 +0100

    tutorapp backend_v1

---
+++ src/main/java/com/backend/tutor_app/TutorAppApplication.java
@@ -0,0 +1,13 @@
+package com.backend.tutor_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/tutor_app/TutorAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.tutor_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
